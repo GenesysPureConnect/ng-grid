53db8ddb090ca3041aec68eb829392e360d91bbf
Daniel White <daniel@whitefamily.in>
2014-03-04 14:27:57 -0500

    reworking sortfn handling to always get called

--- src/services/SortService.js
+++ src/services/SortService.js
@@ -28,10 +28,19 @@
     };
     //#region Sorting Functions
     sortService.basicSort = function(a, b) {
-        if (a === b) {
+        if (!b && !a) {
             return 0;
         }
-        if (a < b) {
+        else if (!a) {
+            return 1;
+        }
+        else if (!b) {
+            return -1;
+        }
+        else if (a === b) {
+            return 0;
+        }
+        else if (a < b) {
             return -1;
         }
         return 1;
@@ -62,11 +71,29 @@
         return numA - numB;
     };
     sortService.sortAlpha = function(a, b) {
+        if (!b && !a) {
+            return 0;
+        }
+        else if (!a) {
+            return 1;
+        }
+        else if (!b) {
+            return -1;
+        }
         var strA = a.toLowerCase(),
             strB = b.toLowerCase();
         return strA === strB ? 0 : (strA < strB ? -1 : 1);
     };
     sortService.sortDate = function(a, b) {
+        if (!b && !a) {
+            return 0;
+        }
+        else if (!a) {
+            return 1;
+        }
+        else if (!b) {
+            return -1;
+        }
         var timeA = a.getTime(),
             timeB = b.getTime();
         return timeA === timeB ? 0 : (timeA < timeB ? -1 : 1);
@@ -107,22 +134,7 @@
                 
                 var propA = $parse(order[indx])(itemA);
                 var propB = $parse(order[indx])(itemB);
-                // we want to allow zero values to be evaluated in the sort function
-                if ((!propA && propA !== 0) || (!propB && propB !== 0)) {
-                    // we want to force nulls and such to the bottom when we sort... which effectively is "greater than"
-                    if (!propB && !propA) {
-                        tem = 0;
-                    }
-                    else if (!propA) {
-                        tem = 1;
-                    }
-                    else if (!propB) {
-                        tem = -1;
-                    }
-                }
-                else {
-                    tem = sortFn(propA, propB);
-                }
+                tem = sortFn(propA, propB);
                 indx++;
             }
             //made it this far, we don't have to worry about null & undefined
